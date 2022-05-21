25277b73ec0ab092ad08bb5274263190b57310fc
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-21 17:17:13 +0100

    static User items rendered in grid forms

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
+import { Users } from "./components/users/Users";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = [
-      { name: "rolodex-1" },
-      { name: "rolodex-2" },
-      { name: "rolodex-3" },
-      { name: "rolodex-4" },
-      { name: "rolodex-5" },
-    ];
-  }
   render() {
     return (
       <div className="App">
         <NavBar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
---
+++ src/components/users/Users.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+import "./usersStyle.css";
+
+export class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "1",
+        login: "mojombo1",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      },
+      {
+        id: "2",
+        login: "mojombo2",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      },
+      {
+        id: "3",
+        login: "mojombo3",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      },
+      {
+        id: "4",
+        login: "mojombo4",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      },
+    ],
+  };
+  render() {
+    // const { avatar_url, login, html_url } = this.state.users;
+
+    return (
+      <div className="users">
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+}
+
+{
+  /* {this.state.users.map((user) => (
+          <div className="card text-center">
+            <img
+              alt="profile pic"
+              src={user.avatar_url}
+              className="round-img"
+              style={{ width: "60px" }}
+            />
+            <h3>{user.login}</h3>
+            <div>
+              <a className="btn btn-dark btn-sm my-1" href={user.html_url}>
+                More{" "}
+              </a>
+            </div>
+          </div>
+        ))} */
+}
