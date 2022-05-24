032ef3b4999ad093ea656bdf4cb5174aea5d614b
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-24 17:51:33 +0100

    Converted spinner to functional component

--- src/components/utils/spinner/Spinner.js
+++ src/components/utils/spinner/Spinner.js
@@ -1,15 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import spinner from "./spinner.gif";
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <Fragment>
-        <img src={spinner} alt="loading" style={spinnerStyle} />
-      </Fragment>
-    );
-  }
-}
+export const Spinner = () => {
+  return (
+    <Fragment>
+      <img src={spinner} alt="loading" style={spinnerStyle} />
+    </Fragment>
+  );
+};
 
 const spinnerStyle = {
   width: "250px",
